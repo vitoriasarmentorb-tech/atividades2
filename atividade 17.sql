47fcf623d4281aff59bc6c3365ced02c70a4f05a
Vitória Sarmento <vitoriasarmentorb@gmail.com>
2026-06-30 16:42:44 -0300

    Add files via upload

---
+++ atividade 17.sql
@@ -0,0 +1,11 @@
+create database conexaophp;
+use conexaophp;
+
+create table calculo(
+id int auto_increment primary key,
+numero1 int not null,
+numero2 int not null,
+resultado int not null
+);
+select * from calculo;
+
