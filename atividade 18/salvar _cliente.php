41f56f05adc3c79d37de47fd5728409166194eae
Vitória Sarmento <vitoriasarmentorb@gmail.com>
2026-07-10 14:43:12 -0300

    Add files via upload

---
+++ atividade 18/salvar _cliente.php
@@ -0,0 +1,18 @@
+<?php
+include "banco.php";
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$cidade = $_POST['cidade'];
+
+$sql = "INSERT INTO form (nome,idade,email,telefone,cidade) 
+VALUES ('$nome', $idade, '$email', '$telefone', '$cidade')";
+
+if ($conexao->query($sql)) {
+    echo "Dados salvos com sucesso!";
+} else {
+    echo "Erro ao salvar os dados!";
+}
+ 
