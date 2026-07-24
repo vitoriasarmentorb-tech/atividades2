eeb819913fb0f9c0b58204a95abae90c7a9ad958
Vitória Sarmento <vitoriasarmentorb@gmail.com>
2026-07-24 15:08:14 -0300

    Add files via upload

---
+++ atividade6.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "7 x 1 = 7 <br>";
+echo "7 x 2 = 14 <br>";
+echo "7 x 3 = 21 <br>";
+echo "7 x 4 = 28 <br>";
+echo "7 x 5 = 35 <br>";
+echo "7 x 6 = 42 <br>";
+echo "7 x 7 = 49 <br>";
+echo "7 x 8 = 56 <br>";
+echo "7 x 9 = 63 <br>";
+echo "7 x 10 = 70 <br>";
+
+?>
